729cc6f31994f2ef567a12ecdddd635081c910b3
Stephen Irons <stephen.irons@clear.net.nz>
2016-06-05 22:28:46 +1200

    Minor refactor.

--- xml2pro.py
+++ xml2pro.py
@@ -1,5 +1,9 @@
+#! /usr/bin/env python3
 
-import sys
+""" Convert MusicXML to Chordpro
+"""
+
+
 import xml.etree.ElementTree as ET
 
 
@@ -36,71 +40,77 @@ keys = {
     7: ('C#', 'A#min'),
 }
 
-fout = sys.stdout
-filename = 'test/An Affair to Remember-jazz.xml'
 
-tree = ET.parse(filename)
-root = tree.getroot()
+def xml2pro(filename, fout):
+    tree = ET.parse(filename)
+    root = tree.getroot()
 
-# Get the song name
-title_element = root.find('work/work-title')
-title = title_element.text
-fout.write('{{title:{title}}}\n'.format(title=title))
+    # Get the song name
+    title_element = root.find('work/work-title')
+    title = title_element.text
+    fout.write('{{title:{title}}}\n'.format(title=title))
 
-# Get a list of all of the parts
-partlists = root.findall('part-list/score-part')
-for part in partlists:
-    part_id = part.attrib['id']
-    part_name = part.find('part-name')
+    # Get a list of all of the parts
+    partlists = root.findall('part-list/score-part')
+    for part in partlists:
+        part_id = part.attrib['id']
+        part_name = part.find('part-name')
 
-# Then ignore the parts in the part list, and just go through the children.
+    # Then ignore the parts in the part list, and just go through the children.
 
-parts = root.findall('part')
-part = parts[0] # This score has a single part. We should be doing this for each part
+    parts = root.findall('part')
+    part = parts[0] # This score has a single part. We should be doing this for each part
 
-# Assume that the first key signature is the key for the song
-key_index = int(part.find('measure/attributes/key/fifths').text)
-key = keys[key_index]
-fout.write('{{key:{kmaj} {kmin}}}\n'.format(kmaj=key[0], kmin=key[1]))
+    # Assume that the first key signature is the key for the song
+    key_index = int(part.find('measure/attributes/key/fifths').text)
+    key = keys[key_index]
+    fout.write('{{key:{kmaj} {kmin}}}\n'.format(kmaj=key[0], kmin=key[1]))
 
-# End of the header info, about to start with the chords and music.
-fout.write('\n')
+    # End of the header info, about to start with the chords and music.
+    fout.write('\n')
 
-# Process all measure in this part.
-measures = part.findall('measure')  # Assume measures are sorted. We should really sort them by attribute 'number'
+    # Process all measure in this part.
+    measures = part.findall('measure')  # Assume measures are sorted. We should really sort them by attribute 'number'
 
 
-# Go through each measure, looking for
-#   'harmony', which tells us the chord to use for the next note
-#   'note', which has a lyric syllable attached to it
+    # Go through each measure, looking for
+    #   'harmony', which tells us the chord to use for the next note
+    #   'note', which has a lyric syllable attached to it
 
-stype = ''  # Type of syllable: single, start, middle or end.
+    stype = ''  # Type of syllable: single, start, middle or end.
 
-for m in measures:
-    measure_number = int(m.get('number'))
-    for child in m:
-        if child.tag == 'harmony':
-            chord_root = child.find('root/root-step').text
-            quality = child.find('kind').text
-            q_code = qualities[quality]
-            chord = chord_root + q_code
+    for m in measures:
+        measure_number = int(m.get('number'))
+        for child in m:
+            if child.tag == 'harmony':
+                chord_root = child.find('root/root-step').text
+                quality = child.find('kind').text
+                q_code = qualities[quality]
+                chord = chord_root + q_code
 
-            # If we have to print a chord in the middle of a word,
-            # insert a dash/hyphen before the chord
-            if stype in ['begin', 'middle']:
-                fout.write('-')
-            fout.write('[{chord}]'.format(chord=chord))
-        elif child.tag == 'note':
-            lyrics = child.findall('lyric[@number="1"]')
-            for l in lyrics:
-                stype = l.find('syllabic').text
-                syllable = l.find('text').text
-                fout.write(syllable)
-                # If this is a single syllable word, or the end of a word, print a space
-                if stype in ['single', 'end']:
-                   fout.write(' ')
+                # If we have to print a chord in the middle of a word,
+                # insert a dash/hyphen before the chord
+                if stype in ['begin', 'middle']:
+                    fout.write('-')
+                fout.write('[{chord}]'.format(chord=chord))
+            elif child.tag == 'note':
+                lyrics = child.findall('lyric[@number="1"]')
+                for l in lyrics:
+                    stype = l.find('syllabic').text
+                    syllable = l.find('text').text
+                    fout.write(syllable)
+                    # If this is a single syllable word, or the end of a word, print a space
+                    if stype in ['single', 'end']:
+                       fout.write(' ')
 
-    # Every 4 bars, start a new line
-    if measure_number % 4 == 0:
-       fout.write('\n')
+        # Every 4 bars, start a new line
+        if measure_number % 4 == 0:
+           fout.write('\n')
+
+
+if __name__ == '__main__':
+    import sys
+    fout = sys.stdout
+    filename = 'test/An Affair to Remember.xml'
+    xml2pro(filename, fout)
 
